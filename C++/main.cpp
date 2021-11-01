f3fd9dbe4a4cace54d9bf3b59ce7b2a5e01835f2
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-01 11:56:54 +0100

    Created main.cpp

---
+++ C++/main.cpp
@@ -0,0 +1,16 @@
+#include "./blockchain/blockchain.cpp"
+#include "./config/config.json"
+#include "./database/database.json"
+#include "./gui/gui.cpp"
+#include "./internet/internet.cpp"
+#include "./wallet/wallet.cpp"
+
+namespace MAIN {
+    
+    int INIT() {
+        
+        
+        
+    };
+    
+}
